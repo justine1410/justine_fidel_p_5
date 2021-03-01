8fca5b8b7cb3ddecba721165d9b74bf6a20f7c60
Justine Fidel <justine1410@live.fr>
2021-03-01 14:43:08 +0100

    recuperation panier

--- js/produit.js
+++ js/produit.js
@@ -12,17 +12,30 @@ const urlTeddy="http://localhost:3000/api/teddies/"+id;
 const urlCam="http://localhost:3000/api/cameras/"+id;
 const urlFurn="http://localhost:3000/api/furniture/"+id;
 
-//promesse teddy
-fetch(urlTeddy)
-.then(async (response)=>{
-    try{
-        resultat= await response.json();
-        console.log(resultat)
-        
-        const listeProduits = document.getElementById("list-produits");
-        const produit=
-        `
-        <div id="produit">
+
+
+ function request(url){
+   fetch(url)
+   .then(async (response) =>{
+     try{
+        resultat = await response.json();
+       console.log(resultat)
+       produit(resultat)
+     }
+     catch (e){
+       console.log(e);
+     }
+   });
+ };
+ request(urlTeddy)
+
+
+ //texte à ajouté
+const choixProduits = document.getElementById("list-produits");
+
+ function produit(){
+     choixProduits.innerHTML = choixProduits.innerHTML+`
+     <div id="produit">
         <img src=${resultat.imageUrl} alt="tedy.1"/>
         <div class="produit-texte">
         <h3 >${resultat.name}</h3>
@@ -38,199 +51,74 @@ fetch(urlTeddy)
         </a>
         </div>
     </div>
-        `
+  ` ;
+   //les options
+  const optionQuantite =resultat.colors;     
+  let structureOptions = [];
+  
+  for (i=0; i<optionQuantite.length; i++){
+      structureOptions =structureOptions+ `
+      <option value=${i}>${optionQuantite[i]}</option>
+      `
+  };
 
-        //formulaire s'adapte au nombre d'option
-        const optionQuantite =resultat.colors;
-        
-        let structureOptions = [];
-        
-        for (i=0; i<optionQuantite.length; i++){
-            structureOptions =structureOptions+ `
-            <option value=${i+1}>${optionQuantite[i]}</option>
-            `
-        }
-    
-        //injection html produit et option
-        listeProduits.innerHTML= produit
+  //injection html produit et option
+  const positionOption = document.querySelector("#perso")
+  positionOption.innerHTML = structureOptions
+   //......récupération des donnée et envoie au panier......................//
+   const idForm = document.querySelector("#perso");
 
-        const positionOption = document.querySelector("#perso")
-        positionOption.innerHTML = structureOptions
+   //séléction du btn ajouter
+   const btnEnvoi = document.querySelector("#btn_envoi")
+   
+    //ecouter le btn et envoyer le panier
+    btnEnvoi.addEventListener("click", (event)=>{
+        event.preventDefault();
 
-//......récupération des donnée et envoie au panier......................//
-        const idForm = document.querySelector("#perso");
-        
-            //séléction du btn ajouter
-        const btnEnvoi = document.querySelector("#btn_envoi")
-        
-            //ecouter le btn et envoyer le panier
-        btnEnvoi.addEventListener("click", (event)=>{
-            event.preventDefault();
-
-            const choixForm = idForm.value;
+        const choixForm =idForm.value;
         console.log(choixForm)
-    
-                        //recuperation des valeurs du formulaire
-            let optionsProduit = {
-                name:resultat.name,  
-                idProduit : resultat._id,  
-                perso:choixForm,
-                quantite: 1,
-                prix:resultat.price/100,
+        //recuperation des valeurs du formulaire
+        optionsProduit = {
+            name:resultat.name,  
+            idProduit : resultat._id,  
+            perso:choixForm,
+            quantite: 1,
+            prix:resultat.price/100,
+        };
+        console.log(btnEnvoi)
+              //..........................Le local Storage.................
+
+              //JSON.parse convertit les données au foramt JSON en objet JAVASCRIPT
+              //variable on on va mettre les key et les values qui sont dans le local storage
+        let produitStorage = JSON.parse (localStorage.getItem("produit"));
+        console.log(produitStorage);
+
+                //fonction fenêtre popup
+            const popConfirm =() =>{
+              if(window.confirm(`${resultat.name} option :${choixForm} a bien été ajouté au panier 
+              Consultez le panier OK ou revenir à l'acceuil ANNULER`)){
+                window.location.href = "panier.html";
+
+              }else{
+                window.location.href = "/index.html";
+              }
             }
-            //console.log(optionsProduit)
-        });
-    } 
-    catch(err){
-        console.log(err)
-        
-    }
-});
-
-/*//promesse camera
-fetch(urlCam)
-.then(async (response)=>{
-    try{
-        resultat= await response.json();
-        console.log(resultat)
-        const listeProduits = document.getElementById("list-produits");
-        const produit=
-        `
-        <div id="produit">
-            <img src=${resultat.imageUrl} alt="tedy.1"/>
-            <div class="produit-texte">
-            <h3 >${resultat.name}</h3>
-            <p >${resultat.price/100}€</p>
-            <p >${resultat.description}</p>
-            <div class=" perso">
-                <label for="perso">Choix de votre couleur:</label><br/>
-                <select name="perso" id="perso">
-                </select>
-            </div>
-            <a class="commande" href="panier.html">
-                <button id="btn_envoi" type="submit"> Ajouter au panier</button>
-            </a>
-            </div>
-        </div>
-        `
-        //formulaire s'adapte au nombre d'option
-        const optionQuantite =resultat.lenses;
-        
-        let structureOptions = [];
-        
-        for (i=0; i<optionQuantite.length; i++){
-            structureOptions =structureOptions+ `
-            <option value=${i+1}>${optionQuantite[i]}</option>
-            `
+                //s'il y a deja de produits d'enregistré
+        if(produitStorage){
+          produitStorage.push(optionsProduit);
+          localStorage.setItem("produit", JSON.stringify(produitStorage));
+          console.log(produitStorage);
+          popConfirm();
+          console.log("ok")
         }
-    
-        //injection html produit et option
-        listeProduits.innerHTML= produit
+        //s'il y a pas de produit
+        else{
+            produitStorage =[];
+            produitStorage.push(optionsProduit);
+            localStorage.setItem("produit", JSON.stringify(produitStorage));
+            popConfirm();
 
-        const positionOption = document.querySelector("#perso")
-        positionOption.innerHTML = structureOptions
-
- //......récupération des donnée et envoie au panier......................//
-        const idForm = document.querySelector("#perso");
-        
-            //séléction du btn ajouter
-        const btnEnvoi = document.querySelector("#btn_envoi")
-        
-            //ecouter le btn et envoyer le panier
-        btnEnvoi.addEventListener("click", (event)=>{
-            event.preventDefault();
-
-            const choixForm = idForm.value;
-            console.log(choixForm)
-    
-                        //recuperation des valeurs du formulaire
-            let optionsProduit = {
-                    name:resultat.name,  
-                    idProduit : resultat._id,  
-                    perso:choixForm,
-                    quantite: 1,
-                    prix:resultat.price/100,
-                }
-            console.log(optionsProduit)
-        });
-    } 
-    catch(err){
-        console.log(err)
-        
-    }
-});
-
-
-//promesse meuble
-fetch(urlFurn)
-.then(async (response)=>{
-    try{
-        resultat= await response.json();
-        console.log(resultat)
-        const listeProduits = document.getElementById("list-produits");
-        const produit=
-        `
-        <div id="produit">
-        <img src=${resultat.imageUrl} alt="tedy.1"/>
-        <div class="produit-texte">
-          <h3 >${resultat.name}</h3>
-          <p >${resultat.price/100}€</p>
-          <p >${resultat.description}</p>
-          <div class=" perso">
-            <label for="perso">Choix de votre couleur:</label><br/>
-            <select name="perso" id="perso">
-            </select>
-          </div>
-          <a class="commande" href="panier.html">
-            <button id="btn_envoi" type="submit"> Ajouter au panier</button>
-          </a>
-        </div>
-      </div>
-        `
-        //formulaire s'adapte au nombre d'option
-        const optionQuantite =resultat.varnish;
-
-        let structureOptions = [];
-        
-        for (i=0; i<optionQuantite.length; i++){
-            structureOptions =structureOptions+ `
-            <option value=${i+1}>${optionQuantite[i]}</option>
-            `
+            console.log(produitStorage)
         }
-    
-        //injection html produit et option
-        listeProduits.innerHTML= produit
-
-        const positionOption = document.querySelector("#perso")
-        positionOption.innerHTML = structureOptions
-        
-
- //......récupération des donnée et envoie au panier......................//
-        const idForm = document.querySelector("#perso");
-        
-            //séléction du btn ajouter
-        const btnEnvoi = document.querySelector("#btn_envoi")
-        
-            //ecouter le btn et envoyer le panier
-        btnEnvoi.addEventListener("click", (event)=>{
-            event.preventDefault();
-
-            const choixForm = idForm.value;
-            console.log(choixForm)
-    
-                        //recuperation des valeurs du formulaire
-            let optionsProduit = {
-                name:resultat.name,  
-                idProduit : resultat._id,  
-                perso:choixForm,
-                quantite: 1,
-                prix:resultat.price/100,
-            }
-            console.log(optionsProduit)
-        });
-    } 
-    catch(err){
-        console.log(err)
-        
-    }
-});*/
+    });
+ };
